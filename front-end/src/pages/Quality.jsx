e03689f5724cb6b040bbc9caafdfbe4ff8c25efe
Debjit Adhikari <debjitadhikari122@gmail.com>
2025-01-27 23:05:44 +0530

    ui updated again

--- front-end/src/pages/Quality.jsx
+++ front-end/src/pages/Quality.jsx
@@ -79,7 +79,7 @@ function Quality() {
               />
               <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center">
                 <p className="text-white text-center px-4">
-                  {item.description || "Equipment Name"}
+                  {item.description || "Equipment Name"} 
                 </p>
               </div>
             </div>
