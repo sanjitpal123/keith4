c1aaf8360580d9b3446dd16490da6c20b718c605
sanjitssp <manjitpal895@gmail.com>
2025-01-27 23:01:00 +0530

    updated fetchHeading

--- front-end/src/services/Homepage/FetchHeading.jsx
+++ front-end/src/services/Homepage/FetchHeading.jsx
@@ -3,9 +3,7 @@ import axios from 'axios';
 
 async function FetchHeroSection() {
     try {
-        const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/hero/get`, {
-            withCredentials: true,  // Ensure cookies are sent with the request
-        });
+        const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/hero/get`);
         
         console.log('res',res)
         return res.data;
