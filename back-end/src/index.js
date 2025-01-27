e96e6a20772e357fdb4b0bb9104d5bda20f8c566
sanjitssp <manjitpal895@gmail.com>
2025-01-27 23:15:22 +0530

    changed cors origin

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://keith-neon.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
