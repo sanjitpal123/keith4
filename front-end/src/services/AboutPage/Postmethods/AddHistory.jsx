51fec85471a5294a511feabbecd49871edabd151
sanjitssp <manjitpal895@gmail.com>
2025-01-30 15:27:56 +0530

    added seo

--- front-end/src/services/AboutPage/Postmethods/AddHistory.jsx
+++ front-end/src/services/AboutPage/Postmethods/AddHistory.jsx
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 async function AddNewHistory(formData) {
-<<<<<<< HEAD
-  try 
-  {
-    console.log(`import.meta.env.VITE_API_BASE_URL`,import.meta.env.VITE_API_BASE_URL)
-=======
+
+ 
   try {
->>>>>>> toasting
+
     const res = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/history/create`,
       formData
     );
     console.log("res", res);
     return res.data;
   } catch (error) {
-    throw new error;
+    throw  error;
   }
 }
 
