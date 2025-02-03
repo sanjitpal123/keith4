fb0e7fedc489466ffd0bd6f9a1dc71a69b1060ce
sanjitssp <manjitpal895@gmail.com>
2025-02-03 23:22:04 +0530

    added navbar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "../styles/Home.css"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
